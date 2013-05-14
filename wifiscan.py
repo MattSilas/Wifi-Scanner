c308fe29ee73ac8a42666de112564cf1f257a9da
Matthew Silas <matthewj.silas@gmail.com>
2013-05-13 22:39:38 -0500

    Added URL parameter method for uploading data to API, JSON currently disabled

--- wifiscan.py
+++ wifiscan.py
@@ -3,6 +3,7 @@
 from commands import getoutput
 import json
 import xml.etree.ElementTree
+import urllib, webbrowser
 
 #Runs OSX Airport utility with scan and xml out flags set
 def find_access_points():
@@ -16,25 +17,43 @@ def find_access_points():
         # 1st string is MAC address
         address = access_point.find("string").text
         # 2nd string is SSID
-        ssid = access_point.findall("string")[1].text
+        #ssid = access_point.findall("string")[1].text
         # 8th integer is signal strength
         strength = abs(int(access_point.findall("integer")[7].text))
-        access_points[address] = strength, ssid
+        access_points[address] = strength
 
     return access_points
 
 #Converts the Airport XML to JSON
 def xml_to_json(signals):
-    data = {"access_points": []}
+    data = {"ap": []}
 
-    for address, attributes in signals.items():
-        ap = {"mac_address": address, "values": attributes}
-        data["access_points"].append(ap)
+    for address, rssi in signals.items():
+        ap = {"mac_address": address, "values": rssi}
+        data["ap"].append(ap)
 
     return json.JSONEncoder().encode(data)
 
+#Encodes the the access point data as parameters to send data
+#to the URL as a GET request. Data2 was created to support the
+#way the API is currently written. Once a decision is made either
+#data2 or data will be destroyed
+def parameters(signals):
+    data = {'MAC':[], 'RSSI':[]}
+    for address, rssi in signals.items():
+        data["MAC"].append(address)
+        data["RSSI"].append(rssi)
+    return urllib.urlencode(data,True).replace("%3A",":")
 
 if __name__ == "__main__":
     print "Finding Access Points"
     access_points = find_access_points()
-    json = xml_to_json(access_points)
+    print "URL encoding the parameters"
+    params = parameters(access_points)
+    url = "http://web.engr.oregonstate.edu/~silasm/CS419/API.php?%s"
+    print params
+    f = urllib.urlopen(url %params)
+    print f.read()
+    webbrowser.open_new_tab(url % params)
+    
+     

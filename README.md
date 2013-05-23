56ef7e5d2107051470165c3633bccb583b62e977
Matthew Silas <matthewj.silas@gmail.com>
2013-05-22 20:59:20 -0500

    Created Windows support

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ Wifi-Scanner
 
 Python Wifi scanner that displays nearby APs and parameterizes the output to send the data to an API in the form
 
+Runs on Windows and OSX
+
 TO-DO
 ============
 
-Add Windows and Linux support
+Add Linux support. Improve Windows parsing or change OSX parsing to adhere to DRY principle
--- wifiscan.py
+++ wifiscan.py
@@ -1,12 +1,13 @@
 #OSX Wifi Scanner
 
-from commands import getoutput
+
 import json, sys
 import xml.etree.ElementTree
 import urllib, webbrowser
 
 #Runs OSX Airport utility with scan and xml out flags set
 def find_access_points_osx():
+    from commands import getoutput
     scan = '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s -x'   
     root = xml.etree.ElementTree.fromstring(getoutput(scan))
     output = root.getchildren()[0]
@@ -23,8 +24,30 @@ def find_access_points_osx():
         access_points[address] = strength
 
     return access_points
-    
 
+#Runs win32 netsh utility and searches for BSSID and RSSI based on regex    
+def find_access_points_win():   
+    scan = 'cmd.exe /c netsh wlan show network mode=Bssid | findstr "BSSID Signal"'
+    root=subprocess.check_output(scan)
+    ap_list = root.decode()
+    mac_address = '([a-fA-F0-9]{1,2}[:]?){6}'
+    c = re.compile(mac_address).finditer(ap_list)
+    access_points={}
+    signals =get_sig_strength_win(ap_list)
+    x=0
+    if c:
+        for y in c:
+            access_points[(ap_list[y.start(): y.end()])]=signals[x]
+            x=x+1
+    return access_points
+  
+#returns array of all RSSIs from scan 
+def get_sig_strength_win(ap_list):
+    sig_strength = r'([\d\d]+)%'
+    c = re.findall(sig_strength,ap_list)
+    if c:
+        return c
+    
 #Converts the Airport XML to JSON, currently not utilized
 def xml_to_json(signals):
     data = {"ap": []}
@@ -50,11 +73,11 @@ if __name__ == "__main__":
         print "no linux support currently"
         exit
     if sys.platform == 'win32':
-        print "no win32 support currently"
-        exit
+        access_points = find_access_points_win32()
+        
     print "Econding Parameters"
     params = parameters(access_points)
-    url = ""
+    url = "http://web.engr.oregonstate.edu/~silasm/CS419/API.php?%s"
 
     f = urllib.urlopen(url %params)
     print "Retrieving JSON"
